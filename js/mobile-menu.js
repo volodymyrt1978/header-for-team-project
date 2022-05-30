6589eb1ee2302dc5387ebac1d63173a63a632c1f
volodymyrt1978 <volodymyr.telyovych@gmail.com>
2022-05-30 17:16:06 +0300

    закінчив js script для мобільного меню

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -12,4 +12,36 @@
         mobileMenuRef.classList.toggle("is-open");
         document.body.classList.toggle("mobile-menu__open");
     });
+
+     const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+    const body = document.querySelector('body');
+    
+    if (menuLinks.length > 0) {
+        menuLinks.forEach(menuLink => {
+            menuLink.addEventListener("click", onMenuLinkClick);
+        });
+
+        function onMenuLinkClick(e) {
+            const menuLink = e.target;
+            if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+                const gotoBlock = document.querySelector(menuLink.dataset.goto);
+                const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY;
+
+               
+                if (body.classList.contains('mobile-menu__open')) {
+                    document.body.classList.remove("mobile-menu__open");
+                    mobileMenuRef.classList.remove("is-open");
+                    menuBtnRef.classList.remove("is-open");
+                }
+                
+                    
+                window.scrollTo({
+                    top: gotoBlockValue,
+                    behavior: "smooth"
+                });
+                e.preventDefault();
+            }
+        }
+    }
+
 })();
